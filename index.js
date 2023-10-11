ab2b3f6b905a8a0caef7356376b91fdd0291b0ec
Deedee <deedeeowoeye@gmail.com>
2023-10-11 09:49:24 +0100

    Added Cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const mongoose = require('mongoose');
 const patients = require("./routes/patientRoutes");
 const dotenv = require("dotenv").config();
@@ -8,7 +9,9 @@ const PORT = process.env.PORT || 3000;
 const mongoConn = process.env.MONGO_CONN;
 
 
+
 // Middleware to parse request bodies
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
